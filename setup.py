a7ec0434f9b8e1ff2e58db2091019c55d2b6d35a
Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>
2020-06-05 21:29:02 +1000

    test update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open('bit/__init__.py', 'r') as f:
             break
 
 setup(
-    name='bit',
+    name='sweet',
     version=version,
-    description='Bitcoin made easy.',
+    description='Sugarchain made easy.',
     long_description=open('README.rst', 'r').read(),
     author='Ofek Lev',
     author_email='ofekmeister@gmail.com',
@@ -20,7 +20,7 @@ setup(
     license='MIT',
 
     keywords=(
-        'bitcoin',
+        'sugarchain',
         'cryptocurrency',
         'payments',
         'tools',
